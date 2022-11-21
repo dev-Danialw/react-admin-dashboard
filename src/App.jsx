d621b4d49193c39931ac308b5d49b54ce3504e03
Danial-Waheed <daniyalwahied@gmail.com>
2022-11-21 18:37:02 +0500

    init with routes setup ✅

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Components
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import Single from "./pages/single/Single";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+
+            {/* Users Routes */}
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+
+            {/* Products Routes */}
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
