6dba8372bd39382deaf0e61b127ab8356cd83dfa
Danial-Waheed <daniyalwahied@gmail.com>
2022-11-21 23:06:53 +0500

    users dataTables

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,7 +1,18 @@
 import "./list.scss";
 
-const List = () => {
-  return <div>List</div>;
-};
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import DataTable from "../../components/datatable/DataTable";
 
+const List = () => {
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <DataTable />
+      </div>
+    </div>
+  );
+};
 export default List;
